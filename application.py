2e8846c2c90dd6c40264640ae228286bc5731d2e
Tobius Saul <30893923+tobiusaolo@users.noreply.github.com>
2019-08-16 09:45:34 +0300

    new stuff

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ from models import *
 # Configure app
 app = Flask(__name__)
 app.secret_key=os.environ.get('SECRET')
-app.config['WTF_CSRF_SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08\xaa"
+#app.config['WTF_CSRF_SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08\xaa"
 
 # Configure database
 app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
